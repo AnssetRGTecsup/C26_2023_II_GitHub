9c11bf3b234b957c002cfdf5155f7202cdb7c9f3
MariaClaverias <130480252+AnssetRGTecsup@users.noreply.github.com>
2023-11-09 10:02:17 -0500

    Player Data created

---
+++ PlayerData.cs
@@ -0,0 +1,9 @@
+public class PlayerData{
+    public int MaxHP;
+    public int CurrentHP;
+
+    public PlayerData(int MaxHP){
+        this.MaxHP = MaxHP;
+        this.CurrentHP = MaxHP;
+    }
+}
