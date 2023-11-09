fd10c55f54b97ad3e3704b2947c198c84f24aa54
Marcosky1 <144145890+Marcosky1@users.noreply.github.com>
2023-11-09 10:14:12 -0500

    Damage

--- PlayerData.cs
+++ PlayerData.cs
@@ -1,9 +1,17 @@
-public class PlayerData{
+public class PlayerData {
     public int MaxHP;
     public int CurrentHP;
+    public int Damage;
+    public int Level;
 
-    public PlayerData(int MaxHP){
+    public PlayerData(int MaxHP) {
         this.MaxHP = MaxHP;
         this.CurrentHP = MaxHP;
+    }
+
+    public NewDamage(int level, int damage)
+    {
+        this.Level = level;
+        this.Damage = damage;
     }
 }
