c33d45d3ecafc712e2618c8851edffaf03517dde
JoaquinCondori <joaquin.condori@tecsup.edu.pe>
2023-11-09 10:13:13 -0500

    Funcion para dañar al jugador

--- PlayerData.cs
+++ PlayerData.cs
@@ -1,9 +1,20 @@
-public class PlayerData{
+public class PlayerData
+{
     public int MaxHP;
     public int CurrentHP;
 
-    public PlayerData(int MaxHP){
+    public PlayerData(int MaxHP)
+    {
         this.MaxHP = MaxHP;
         this.CurrentHP = MaxHP;
     }
+
+    public void DamagePlayer(int damage) 
+    {
+    CurrentHP -= damage;
+    if (CurrentHP < 0) 
+    {
+        CurrentHP = 0;
+    }
+}
 }
